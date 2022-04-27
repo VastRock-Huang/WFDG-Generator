dfd82e725c2e64fae2529d6959c3d0a00915d527
Unravel <1621690242@qq.com>
2022-04-27 19:30:33 +0800

    add '-w' command line param

--- apps/main.cpp
+++ apps/main.cpp
@@ -15,16 +15,16 @@ using namespace llvm;
 int main(int argc, const char **argv) {
     // 构建命令行选项类别
     cl::OptionCategory opCategory("WFDG Generation");
-    cl::opt<string> destFunc("f", cl::desc("the function to generate WFDG"), cl::value_desc("funcName"),
+    cl::opt <string> destFunc("f", cl::desc("the function to generate WFDG"), cl::value_desc("funcName"),
                               cl::cat(opCategory));
     cl::opt<unsigned> sensitiveLine("s", cl::desc("the sensitive line of the function"),
                                     cl::value_desc("sensitiveLine"),
                                     cl::cat(opCategory));
-    cl::opt<unsigned> useOptimization("o", cl::desc("use the optimization: remove small graphs"),
-                                      cl::value_desc("optimization"),
-                                      cl::cat(opCategory), cl::init(true));
+    cl::opt<bool> useOptimization("o", cl::desc("use the optimization: remove small graphs"),
+                                  cl::cat(opCategory), cl::init(true));
     cl::opt<bool> showFuncInfo("i", cl::desc("show all FuncInfo"), cl::cat(opCategory));
     cl::opt<bool> debug("d", cl::desc("show debug messages"), cl::cat(opCategory));
+    cl::opt<bool> warnInDiag("w", cl::desc("show diagnostic information from compiler"), cl::cat(opCategory));
     CommonOptionsParser op(argc, argv, opCategory);
 
     vector<CustomCPG> customCPGList{};
@@ -34,14 +34,16 @@ int main(int argc, const char **argv) {
     config.specifyFunc(destFunc, sensitiveLine, false);
 
     ClangTool tool(op.getCompilations(), op.getSourcePathList());
-    // 取消错误输出
-    class : public DiagnosticConsumer {
-    public:
-        virtual bool IncludeInDiagnosticCounts() const {
-            return false;
-        }
-    } diagConsumer;
-    tool.setDiagnosticConsumer(&diagConsumer);
+    if (!warnInDiag) {
+        // 取消错误输出
+        class : public DiagnosticConsumer {
+        public:
+            virtual bool IncludeInDiagnosticCounts() const {
+                return false;
+            }
+        } diagConsumer;
+        tool.setDiagnosticConsumer(&diagConsumer);
+    }
     int ret = tool.run(unique_ptr<CPGGenFactory>(new CPGGenFactory(config, customCPGList)).get());
     for (auto &cpg: customCPGList) {
         if (showFuncInfo) {
